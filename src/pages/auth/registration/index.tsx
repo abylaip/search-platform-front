0963d47280edbfba66c9afa13de528c8c1ab58e8
Abylay Aiyp <ablyaip@gmail.com>
2023-05-01 18:56:01 +0600

    feat: verification code

--- src/pages/auth/registration/index.tsx
+++ src/pages/auth/registration/index.tsx
@@ -1,13 +1,24 @@
 import { useState, useLayoutEffect } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 const Registration = () => {
   const router = useRouter();
-  const [code, setCode] = useState("");
+  const [verificationResponse, setVerificationResponse] = useState("");
+  const sendVerification = async (code: string | string[] | undefined) => {
+    const res = await axios.get(
+      `https://134b-62-84-32-239.ngrok-free.app/verification/verify?code=${code}`
+    );
+    if (res.status === 200) {
+      setVerificationResponse("Верификация прошла");
+    } else {
+      setVerificationResponse("Не получилось верифицировать");
+    }
+  };
   useLayoutEffect(() => {
-    console.log(router.query.code);
+    sendVerification(router.query.code);
   }, []);
-  return <div>{router.query.code}</div>;
+  return <div>{verificationResponse}</div>;
 };
 
 export default Registration;
