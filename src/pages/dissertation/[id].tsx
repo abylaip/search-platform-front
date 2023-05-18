7b5b2dc1e4cc7180048c94028b6a7658450cc1d5
Abylay Aiyp <ablyaip@gmail.com>
2023-05-18 09:38:21 +0600

    layout: profile

--- src/pages/dissertation/[id].tsx
+++ src/pages/dissertation/[id].tsx
@@ -16,15 +16,17 @@ const DissertationPage = () => {
           <span className="font-semibold text-accent">Дипломная работа</span>
         </p>
         <section className="flex flex-col space-y-4 rounded-lg bg-white shadow-lg py-5 px-7">
-          <p className="text-4xl font-bold text-accent">{data?.name}</p>
-          <p className="text-2xl">{data?.organizationName}</p>
-          <p className="text-2xl">
+          <p className="text-2xl font-bold text-accent">{data?.name}</p>
+          <p className="text-xl">{data?.organizationName}</p>
+          <p className="text-xl">
             Категория:{" "}
             <span className="font-semibold text-primary">{data?.category}</span>
           </p>
           <div className="flex flex-col">
-            <p className="text-xl text-accent font-semibold">Абстракт:</p>
-            <p>{data?.dissertAbstract}</p>
+            <p>
+              <span className="font-bold">Abstract. </span>
+              {data?.dissertAbstract}
+            </p>
           </div>
         </section>
       </div>
