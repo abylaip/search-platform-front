8e612595407d7369f7e94952f4028481ca5f0c56
Abylay Aiyp <ablyaip@gmail.com>
2023-05-30 00:58:40 +0600

    saasd

--- src/pages/dissertation/[id].tsx
+++ src/pages/dissertation/[id].tsx
@@ -46,34 +46,49 @@ const DissertationPage = () => {
               </div>
             </div>
             <div className="flex-1 pt-8">
-              <ul>
-                <li className="flex flex-row justify-around mb-3">
-                  <p>Название</p>
-                  <p>Тип файла</p>
-                  <p>Скачать</p>
-                </li>
-                {data?.files.map((item, key) => (
-                  <li className="flex flex-row justify-around" key={key}>
-                    <p>{item.name}</p>·<p>{item.mimeType}</p>·
-                    <button className="text-blue-500">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                        />
-                      </svg>
-                    </button>
-                  </li>
-                ))}
-              </ul>
+              <table className="table-fixed">
+                <tbody>
+                  <tr>
+                    <td>
+                      <p>Название</p>
+                    </td>
+                    <td>
+                      <p>Тип файла</p>
+                    </td>
+                    <td>
+                      <p>Скачать</p>
+                    </td>
+                  </tr>
+                  {data?.files.map((item, key) => (
+                    <tr key={key}>
+                      <td>
+                        <p>{item.name}</p>
+                      </td>
+                      <td>
+                        <p>{item.mimeType}</p>
+                      </td>
+                      <td>
+                        <button className="text-blue-500">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                            />
+                          </svg>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         ) : (
